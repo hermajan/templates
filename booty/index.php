581236ab65b87a6e2b6ecb34d568025d40e0848f
Jan Hermann <hermajan@gmail.com>
2017-08-16 23:58:09 +0200

    Added Booty template.

---
+++ booty/index.php
@@ -0,0 +1,26 @@
+<?php
+require_once __DIR__."/../libs/autoload.php";
+
+$latte = new Latte\Engine;
+$latte->setTempDirectory(__DIR__."/../_temp");
+
+$parameters = [
+	"weburl" => "/github/templates/booty",
+	"webName" => "Booty",
+	
+	"description" => "",
+	"author" => "hermajan",
+	"title" => "Booty",
+	
+	"headerFile" => "_header.latte",
+	"headTag" => "",
+	"searchForm" => "",
+
+	"menuExists" => true,
+	"menuFile" => "_menu.latte",
+
+	"footerFile" => "_footer.latte",
+	"footerContent" => ""
+];
+
+$latte->render("default.latte", $parameters);
